936ec0ba9dacbceb490034be0785a4cf0a3c0953
wgardner6 <wgardner6@gmail.com>
2014-02-14 23:02:02 -0600

    Working on constructors and getDayOfWeek

--- newDayClass/Day.java
+++ newDayClass/Day.java
@@ -4,10 +4,69 @@ private static ArrayList<int> endOfMonth = new ArrayList<int>();
 private static ArrayList<int> beginningOfMonth = new ArrayList<int>();
 private static ArrayList<int> endOfMonthLeap = new ArrayList<int>();
 private static ArrayList<int> beginningOfMonthLeap = new ArrayList<int>();
+private int day;
+private int month;
+private int year;
+/**
+	Constructs a day with user inputted day, month, and year.
+	@param day Day between 1 and 31.
+	@param month Month between 1 and 12
+	@param year Year not equal to zero
 
-public Day(int day, int month, int year)
-Day(Day)
-Day() //returns today's date
+*/
+public Day(int day, int month, int year){
+	day = day;
+	month = month;
+	year = year;
+}
+/**
+	Copy constructor. 
+	@param other Separate instance of Day.
+*/
+public Day(Day other){
+	this.day = other.day;
+	this.month = other.month;
+	this.year = other.year;
+}
+
+public int getDayOfWeek(){
+	int[] monthTable;
+	monthTable = new int[12];
+	if this.isLeapYear(){
+		monthTable[0] = 0;
+		monthTable[1] = 3;
+		monthTable[2] = 3;
+		monthTable[3] = 6;
+		monthTable[4] = 1;
+		monthTable[5] = 4;
+		monthTable[6] = 6;
+		monthTable[7] = 2;
+		monthTable[8] = 5;
+		monthTable[9] = 0;
+		monthTable[10] = 3;
+		monthTable[11] = 5; 
+	}
+	else {
+		monthTable[0] = -1;
+		monthTable[1] = 2;
+		monthTable[2] = 3;
+		monthTable[3] = 6;
+		monthTable[4] = 1;
+		monthTable[5] = 4;
+		monthTable[6] = 6;
+		monthTable[7] = 2;
+		monthTable[8] = 5;
+		monthTable[9] = 0;
+		monthTable[10] = 3;
+		monthTable[11] = 5; 
+	}
+	int d = day;
+	int m = monthTable[month-1];
+	
+}
+
+//Day() //returns today's date
+/**
 int getDayOfWeek() // Use “Principle of Least Surprise” 0-6 or 1-7 which is similar to Python and C
 boolean isLeapYear()
 boolean equals(Day other)
@@ -32,7 +91,7 @@ boolean isHoliday()
 int compareTo(Day other)//if this is less than other return negative. 0 if equal.
 string toString() 
 
-
+*/
 
 
 
